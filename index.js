a35685c53ee3d05c05a035c7191005b37feb5238
AdditionalTesting <albert.burc@gmail.com>
2018-02-02 00:04:27 -0500

    added route for todos list

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 var express = require('express'),
     app = express();
 
+var todoRoutes = require('./routes/todos');
+
 app.get('/', function(req, res){
     res.send("HI THERE FROM EXPRESS!!!")
 })
+
+app.use('/api/todos', todoRoutes)
 
 app.listen(3000, function(){
     console.log("APP IS RUNNING")
